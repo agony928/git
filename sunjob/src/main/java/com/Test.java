92ed1780f47307d59d34c0b29aa3196d14f6bfba
agony928 <2330633655@qq.com>
2020-06-09 19:45:30 +0800

    sunjob

--- sunjob/src/main/java/com/Test.java
+++ sunjob/src/main/java/com/Test.java
@@ -3,6 +3,7 @@ package com;
 public class Test {
     public static void main(String[] args) {
         System.out.println("999感冒灵");
-        System.out.println("1111111");
+        System.out.println("12");
+
     }
 }
